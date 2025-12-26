bc964696c215fd65fc680f291c305df257843339
Jitendra Sachwani(DeskPC) <sachwani.jitendra@gmail.com>
2025-12-26 16:35:41 +0530

    Add provider variables in the module

--- 10_terraform/modules/proxmox_vm/variables.tf
+++ 10_terraform/modules/proxmox_vm/variables.tf
@@ -72,3 +72,18 @@ variable "os_type" {
   type    = string
   default = "l26"
 }
+
+variable "proxmox_endpoint" {
+  type = string
+  default = "https://192.168.1.2:8006/api2/json"
+}
+
+variable "proxmox_username" {
+  type = string
+  default = "root@pam"
+}
+
+variable "proxmox_password" {
+  type      = string
+  sensitive = true
+}
