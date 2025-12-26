026b9d6443f6eff7f9f8f32d1e64402155fccac5
Jitendra Sachwani(DeskPC) <sachwani.jitendra@gmail.com>
2025-12-26 18:54:46 +0530

    Restructure media vms

--- 10_terraform/envs/prod/media_mgmt_01.tf
+++ 10_terraform/envs/prod/media_mgmt_01.tf
@@ -1,16 +1,16 @@
-module "media_arr_01" {
+module "media_mgmt_01" {
   source = "../../modules/proxmox_vm"
 
   providers = {
     proxmox = proxmox
   }
 
-  name  = "prod-media-arr-01"
+  name  = "prod-media-mgmt-01"
   vm_id = 50201
   tags = ["role-media"]
   
   cores = 2
-  memory_mb = 2048
+  memory_mb = 3072
 
   cloud_init_file_id = proxmox_virtual_environment_file.cloud_init_file.id
   import_disk_id = proxmox_virtual_environment_download_file.ubuntu_22_jammy_qcow2.id
--- 10_terraform/envs/prod/media_srv_01.tf
+++ 10_terraform/envs/prod/media_srv_01.tf
@@ -1,16 +1,16 @@
-module "media_dl_01" {
+module "media_srv_01" {
   source = "../../modules/proxmox_vm"
 
   providers = {
     proxmox = proxmox
   }
 
-  name  = "prod-media-dl-01"
-  vm_id = 50101
+  name  = "prod-media-srv-01"
+  vm_id = 50001
   tags = ["role-media"]
   
-  cores = 2
-  memory_mb = 1024
+  cores = 4
+  memory_mb = 3072
 
   cloud_init_file_id = proxmox_virtual_environment_file.cloud_init_file.id
   import_disk_id = proxmox_virtual_environment_download_file.ubuntu_22_jammy_qcow2.id
