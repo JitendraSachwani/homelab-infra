ad3e20a2a0e13194e3f99f203943013e117d92d1
Jitendra Sachwani(DeskPC) <sachwani.jitendra@gmail.com>
2025-12-26 18:55:59 +0530

    Restructure observability vms

--- 10_terraform/envs/prod/observability_01.tf
+++ 10_terraform/envs/prod/observability_01.tf
@@ -1,16 +1,16 @@
-module "monitoring_01" {
+module "observability_01" {
   source = "../../modules/proxmox_vm"
 
   providers = {
     proxmox = proxmox
   }
 
-  name  = "prod-monitoring-01"
+  name  = "prod-observability-01"
   vm_id = 30001
-  tags = ["role-monitoring"]
+  tags = ["role-observability"]
   
   cores = 2
-  memory_mb = 1024
+  memory_mb = 2048
 
   cloud_init_file_id = proxmox_virtual_environment_file.cloud_init_file.id
   import_disk_id = proxmox_virtual_environment_download_file.ubuntu_22_jammy_qcow2.id
