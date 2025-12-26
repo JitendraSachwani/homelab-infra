7e6434c4ddb89b7904d97681f085f1d52f8a0166
Jitendra Sachwani(DeskPC) <sachwani.jitendra@gmail.com>
2025-12-26 17:55:10 +0530

    Cleanup

--- 10_terraform/envs/prod/_images.tf
+++ 10_terraform/envs/prod/_images.tf
@@ -8,38 +8,3 @@ resource "proxmox_virtual_environment_download_file" "ubuntu_22_jammy_qcow2" {
   # need to rename the file to *.qcow2 to indicate the actual file format for import
   file_name = "ubuntu-22-jammy-cloudimg-amd64.qcow2"
 }
-
-resource "proxmox_virtual_environment_file" "ci_runner_cloud_init" {
-  content_type = "snippets"
-  datastore_id = local.default_datastore
-  node_name    = local.default_node
-
-  source_raw {
-    file_name = "prod-ci-runner-01-cloud-init.yaml"
-    data = <<-EOF
-#cloud-config
-hostname: prod-ci-runner-01
-timezone: Asia/Kolkata
-
-users:
-  - name: iac
-    groups: [sudo]
-    shell: /bin/bash
-    ssh_authorized_keys:
-      - ${var.admin_ssh_public_key}
-      - ${var.iac_ssh_public_key}
-    sudo: ALL=(ALL) NOPASSWD:ALL
-
-package_update: true
-packages:
-  - qemu-guest-agent
-  - curl
-  - git
-  - ca-certificates
-
-runcmd:
-  - systemctl enable --now qemu-guest-agent
-  - echo "cloud-init complete" > /var/log/cloud-init.done
-EOF
-  }
-}
