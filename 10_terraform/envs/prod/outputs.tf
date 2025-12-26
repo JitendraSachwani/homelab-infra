bf14e75b73bb32fbccab5a70df4dda444d40b53d
Jitendra Sachwani(DeskPC) <sachwani.jitendra@gmail.com>
2025-12-26 17:06:53 +0530

    Fix outputs

--- 10_terraform/envs/prod/outputs.tf
+++ 10_terraform/envs/prod/outputs.tf
@@ -1,8 +1,7 @@
 output "ansible_hosts" {
   value = {
     ci_runners = {
-      module.ci_runner_01.name => module.ci_runner_01.ipv4_address
-      module.ci_runner_02.name => module.ci_runner_02.ipv4_address
+      module.ci_runner_02.name = module.ci_runner_02.ipv4_address
     }
   }
 }
