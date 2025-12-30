41b4b51926bd8490d899761dec806ffcc70e406f
Jitendra Sachwani(DeskPC) <sachwani.jitendra@gmail.com>
2025-12-31 02:50:35 +0530

    Update lxc outputs

--- 10_terraform/modules/proxmox_lxc/outputs.tf
+++ 10_terraform/modules/proxmox_lxc/outputs.tf
@@ -1,7 +1,7 @@
 output "ipv4_address" {
   description = "Primary IPv4 address of the VM"
   value       = try(
-    proxmox_virtual_environment_container.this.ipv4[1][0],
+    proxmox_virtual_environment_container.this.ipv4[0],
     null
   )
 }
