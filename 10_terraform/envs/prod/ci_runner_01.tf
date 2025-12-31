af2f3aa7a7dbd1725d44e03e3a00c66b79a91843
Jitendra Sachwani(DeskPC) <sachwani.jitendra@gmail.com>
2025-12-31 07:04:48 +0530

    Make ci runner be dhcp

--- 10_terraform/envs/prod/ci_runner_01.tf
+++ 10_terraform/envs/prod/ci_runner_01.tf
@@ -9,9 +9,6 @@ module "ci_runner_01" {
   vm_id = 10301
   tags = ["role-ci"]
 
-  ipv4_address = "10.0.1.3/16"
-  ipv4_gateway = "10.0.0.1"
-
   cloud_init_file_id = proxmox_virtual_environment_file.cloud_init_file.id
   import_disk_id = proxmox_virtual_environment_download_file.ubuntu_22_jammy_qcow2.id
   
