7e359ec6dbf3ff83504d2ca80cfd045de6e562fc
Jitendra Sachwani(DeskPC) <sachwani.jitendra@gmail.com>
2026-01-14 14:16:52 +0530

    Update the oci module with proper names

--- 10_terraform/modules/oracle_cloud/main.tf
+++ 10_terraform/modules/oracle_cloud/main.tf
@@ -2,12 +2,12 @@ resource "oci_identity_compartment" "homelab_tf_compartment" {
   # https://docs.oracle.com/en-us/iaas/Content/dev/terraform/tutorials/tf-compartment.htm#gather-info
   compartment_id = var.oci_tenancy_ocid
 
-  name          = "homelab_tf"
+  name          = "homelab_tf_compartment"
   description   = "Compartment for Terraform managed resources."
   enable_delete = false
 }
 
-resource "oci_core_vcn" "test_vcn" {
+resource "oci_core_vcn" "homelab_tf_vcn" {
   compartment_id = oci_identity_compartment.homelab_tf_compartment.id
   display_name   = "homelab_tf_vcn"
   dns_label      = "tfvcn"
