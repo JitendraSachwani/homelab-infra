27d64769504cbecd3dda57e606175fb7fa30e8a5
Jitendra Sachwani(DeskPC) <sachwani.jitendra@gmail.com>
2026-01-14 14:35:46 +0530

    Configure subnets for the vcn

--- 10_terraform/modules/oracle_cloud/_variables.tf
+++ 10_terraform/modules/oracle_cloud/_variables.tf
@@ -8,3 +8,15 @@ variable "oci_vcn_cidr_blocks" {
   type        = list(string)
   default     = ["10.0.0.0/16"]
 }
+
+variable "oci_public_subnet_cidr_block" {
+  description = "The CIDR for the Public Subnet of the vcn."
+  type        = string
+  default     = "10.0.0.0/24"
+}
+
+variable "oci_private_subnet_cidr_block" {
+  description = "The CIDR for the Private Subnet of the vcn."
+  type        = string
+  default     = "10.0.1.0/24"
+}
--- 10_terraform/modules/oracle_cloud/main.tf
+++ 10_terraform/modules/oracle_cloud/main.tf
@@ -13,3 +13,19 @@ resource "oci_core_vcn" "homelab_tf_vcn" {
   dns_label      = "tfvcn"
   cidr_blocks    = var.oci_vcn_cidr_blocks
 }
+
+resource "oci_core_subnet" "tf_public_subnet" {
+    compartment_id = oci_identity_compartment.homelab_tf_compartment.id
+    vcn_id = oci_core_vcn.homelab_tf_vcn.id
+    display_name = "tf_public_subnet"
+    dns_label = "tfpubsubnet"
+    cidr_block = var.oci_public_subnet_cidr_block       
+}
+
+resource "oci_core_subnet" "tf_private_subnet" {
+    compartment_id = oci_identity_compartment.homelab_tf_compartment.id
+    vcn_id = oci_core_vcn.homelab_tf_vcn.id
+    display_name = "tf_private_subnet"
+    dns_label = "tfpubsubnet"
+    cidr_block = var.oci_private_subnet_cidr_block
+}
