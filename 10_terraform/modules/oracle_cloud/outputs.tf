17139705ecefa74ce429f241f12ea9bd189f49aa
Jitendra Sachwani(DeskPC) <sachwani.jitendra@gmail.com>
2026-01-14 12:33:02 +0530

    Update provider source

--- 10_terraform/modules/oracle_cloud/outputs.tf
+++ 10_terraform/modules/oracle_cloud/outputs.tf
@@ -1,10 +1,10 @@
-output "instance_private_ips" {
-  value = ["${oci_core_instance.TFInstance.*.private_ip}"]
-}
+# output "instance_private_ips" {
+#   value = ["${oci_core_instance.TFInstance.*.private_ip}"]
+# }
 
-output "instance_public_ips" {
-  value = ["${oci_core_instance.TFInstance.*.public_ip}"]
-}
+# output "instance_public_ips" {
+#   value = ["${oci_core_instance.TFInstance.*.public_ip}"]
+# }
 
 # output "instance_id" {
 #   value = oci_core_instance.gateway.id
