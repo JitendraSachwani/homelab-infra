51640522635ba2ac7678a2ff2d149457c50f4bc9
Jitendra Sachwani(DeskPC) <sachwani.jitendra@gmail.com>
2026-01-16 05:36:01 +0530

    update the oci tf module

--- 10_terraform/modules/oracle_cloud/main.tf
+++ 10_terraform/modules/oracle_cloud/main.tf
@@ -15,17 +15,17 @@ resource "oci_core_vcn" "homelab_tf_vcn" {
 }
 
 resource "oci_core_subnet" "tf_public_subnet" {
-    compartment_id = oci_identity_compartment.homelab_tf_compartment.id
-    vcn_id = oci_core_vcn.homelab_tf_vcn.id
-    display_name = "tf_public_subnet"
-    dns_label = "tfpubsubnet"
-    cidr_block = var.oci_public_subnet_cidr_block       
+  compartment_id = oci_identity_compartment.homelab_tf_compartment.id
+  vcn_id         = oci_core_vcn.homelab_tf_vcn.id
+  display_name   = "tf_public_subnet"
+  dns_label      = "tfpubsubnet"
+  cidr_block     = var.oci_public_subnet_cidr_block
 }
 
 resource "oci_core_subnet" "tf_private_subnet" {
-    compartment_id = oci_identity_compartment.homelab_tf_compartment.id
-    vcn_id = oci_core_vcn.homelab_tf_vcn.id
-    display_name = "tf_private_subnet"
-    dns_label = "tfprivsubnet"
-    cidr_block = var.oci_private_subnet_cidr_block
+  compartment_id = oci_identity_compartment.homelab_tf_compartment.id
+  vcn_id         = oci_core_vcn.homelab_tf_vcn.id
+  display_name   = "tf_private_subnet"
+  dns_label      = "tfprivsubnet"
+  cidr_block     = var.oci_private_subnet_cidr_block
 }
