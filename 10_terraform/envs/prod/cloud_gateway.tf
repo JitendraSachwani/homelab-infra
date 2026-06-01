cdc3cde336f7e2119664aa7cbf6f8239acb7b57d
Jitendra Sachwani(DeskPC) <sachwani.jitendra@gmail.com>
2026-06-01 16:42:55 +0530

    try bigger cpu

--- 10_terraform/envs/prod/cloud_gateway.tf
+++ 10_terraform/envs/prod/cloud_gateway.tf
@@ -7,13 +7,11 @@ module "cloud_gateway" {
   oci_tenancy_ocid = var.oci_tenancy_ocid
   gateway_name     = "prod-cloud-gateway-01"
 
-  gateway_shape     = "VM.Standard.E2.1.Micro"
   gateway_ocpus     = 1
-  gateway_memory_gb = 1
-
-  # gateway_shape     = "VM.Standard.A1.Flex"
-  # gateway_ocpus     = 1
-  # gateway_memory_gb = 6
+  # gateway_shape     = "VM.Standard.E2.1.Micro"
+  # gateway_memory_gb = 1
+  gateway_shape     = "VM.Standard.A1.Flex"
+  gateway_memory_gb = 6
 
   iac_ssh_public_key   = var.iac_ssh_public_key
   admin_ssh_public_key = var.admin_ssh_public_key
